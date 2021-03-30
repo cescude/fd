d3cd02c5272ad8b14a0291bc4d68de4fe732ae85
Chandler Escude <chandler.escude@gmail.com>
2021-03-30 12:57:33 -0400

    Working on arguments

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,189 @@ const sep = std.fs.path.sep_str;
 
 const Config = struct {
     use_color: bool = true,
+    files_only: bool = false,
 };
+
+const Args = struct {
+    allocator: *std.mem.Allocator,
+
+    values: std.ArrayList([]const u8), // Backing array for string arguments
+    positionals: std.ArrayList([]const u8), // Backing array for positional arguments
+
+    args: std.ArrayList(Arg), // List of argument patterns
+
+    const Self = @This();
+
+    // How to handle Optionals?
+    const Arg = struct {
+        long_name: ?[]const u8,
+        short_name: ?u8,
+        description: []const u8,
+        val_ptr: union(enum) {
+            Bool: *bool,
+            Str: *[]const u8,
+        },
+    };
+
+    pub fn init(allocator: *std.mem.Allocator) Self {
+        return .{
+            .allocator = allocator,
+            .values = std.ArrayList([]const u8).init(allocator),
+            .positionals = std.ArrayList([]const u8).init(allocator),
+            .args = std.ArrayList(Arg).init(allocator),
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        for (self.values.items) |str| {
+            self.allocator.free(str);
+        }
+        self.values.deinit();
+
+        for (self.positionals.items) |str| {
+            self.allocator.free(str);
+        }
+        self.positionals.deinit();
+
+        self.args.deinit();
+    }
+
+    pub fn boolean(self: *Self, long: ?[]const u8, short: ?u8, ptr: *bool, desc: []const u8) !void {
+        try self.args.append(Arg{
+            .long_name = long,
+            .short_name = short,
+            .description = desc,
+            .val_ptr = .{ .Bool = ptr },
+        });
+    }
+
+    pub fn string(self: *Self, long: ?[]const u8, short: ?u8, ptr: *[]const u8, desc: []const u8) !void {
+        try self.args.append(Arg{
+            .long_name = long,
+            .short_name = short,
+            .description = desc,
+            .val_ptr = .{ .Str = ptr },
+        });
+    }
+
+    pub fn positionals(self: *Self) [][]const u8 {
+        return self.positionals.items;
+    }
+
+    pub fn processCommandLine(self: *Self) !void {
+        var argv = try std.process.argsAlloc(self.allocator);
+        defer std.process.argsFree(self.allocator, argv);
+        try process(argv);
+    }
+
+    pub fn process(self: *Self, argv: [][]const u8) !void {
+        var no_more_flags = false;
+
+        var idx: usize = 0;
+        while (idx < argv.len) : (idx += 1) {
+            var token = argv[idx];
+
+            if (no_more_flags) {
+                std.debug.print("Positional {s}\n", .{token});
+            } else {
+                if (std.mem.eql(u8, token, "--")) {
+                    no_more_flags = true;
+                } else if (std.mem.startsWith(u8, token, "--")) {
+                    try self.fillLongValue(token[2..], argv[idx + 1 ..]);
+                } else if (std.mem.startsWith(u8, token, "-")) {
+                    std.debug.print("Short opt {s}\n", .{token});
+                } else {
+                    std.debug.print("Positional {s}\n", .{token});
+                }
+            }
+        }
+    }
+
+    fn extractName(token: []const u8) []const u8 {
+        if (std.mem.indexOf(u8, token, "=")) |idx| {
+            return token[0..idx];
+        } else {
+            return token;
+        }
+    }
+
+    fn extractValue(token: []const u8, remainder: [][]const u8) ?[]const u8 {
+        if (std.mem.indexOf(u8, token, "=")) |idx| {
+            return token[idx + 1 ..];
+        } else if (remainder.len > 0 and !std.mem.startsWith(u8, remainder[0], "-")) {
+            return remainder[0];
+        } else {
+            return null;
+        }
+    }
+
+    fn findLongArg(args: []Arg, name: []const u8) ?Arg {
+        for (args) |arg| {
+            if (arg.long_name) |long_name| {
+                if (std.mem.eql(u8, long_name, name)) {
+                    return arg;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    fn fillLongValue(self: *Self, token: []const u8, remainder: [][]const u8) !void {
+        var name = extractName(token);
+
+        var arg: Arg = findLongArg(self.args.items, name) orelse return error.UnrecognizedOptionName;
+
+        switch (arg.val_ptr) {
+            .Bool => |ptr| {
+                var value = extractValue(token, remainder) orelse "yes";
+
+                if (std.mem.eql(u8, value, "yes") or
+                    std.mem.eql(u8, value, "true") or
+                    std.mem.eql(u8, value, "y") or
+                    std.mem.eql(u8, value, "on") or
+                    std.mem.eql(u8, value, "1"))
+                {
+                    ptr.* = true;
+                } else if (std.mem.eql(u8, value, "no") or
+                    std.mem.eql(u8, value, "false") or
+                    std.mem.eql(u8, value, "n") or
+                    std.mem.eql(u8, value, "off") or
+                    std.mem.eql(u8, value, "0"))
+                {
+                    ptr.* = false;
+                } else {
+                    return error.BadBooleanValue;
+                }
+            },
+            .Str => |ptr| {
+                var value = extractValue(token, remainder) orelse return error.MissingStringValue;
+                ptr.* = try self.allocator.dupe(u8, value);
+                errdefer self.allocator.free(ptr.*);
+
+                try self.values.append(ptr.*); // Track this string to free on deinit
+            },
+        }
+    }
+};
+
+const expect = std.testing.expect;
+
+test "args" {
+    var args = Args.init(std.testing.allocator);
+    defer args.deinit();
+
+    var first: bool = undefined;
+    var name: []const u8 = undefined;
+
+    try args.boolean("first", 'f', &first, "First option");
+    try args.string("name", 'n', &name, "Give a name!");
+
+    var my_args = [_][]const u8{ "--first", "off", "--first=yes", "name", "two", "-f=yep" };
+
+    try args.process(my_args[0..]);
+    std.debug.print("first={s}, name={s}\n", .{ first, name });
+}
 
 pub fn main() !void {
     var buffer: [4096]u8 = undefined;
@@ -25,6 +207,10 @@ pub fn main() !void {
         .ReleaseFast => std.heap.c_allocator,
         else => &gpa.allocator,
     };
+
+    var args = Args.init(allocator);
+    defer args.deinit();
+    try args.process();
 
     var proc = Proc(@TypeOf(writer)).init(allocator, cfg, &writer);
     try proc.run(cwd);
@@ -70,21 +256,43 @@ fn entryGt(v: void, e0: Entry, e1: Entry) bool {
     return strGt(v, e0.name, e1.name);
 }
 
-test "Sorting functions don't crash on >1024 items" {
-    var items: [1024][]const u8 = undefined;
-    for (items) |_, idx| {
-        items[idx] = try std.fmt.allocPrint(std.testing.allocator, "{d}", .{idx});
-    }
-    defer for (items) |i| {
-        std.testing.allocator.free(i);
-    };
+// test "Sorting functions don't crash on 1024+ items" {
+//     var items: [1024][]const u8 = undefined;
+//     for (items) |_, idx| {
+//         items[idx] = try std.fmt.allocPrint(std.testing.allocator, "{d}", .{idx});
+//     }
+//     defer for (items) |i| {
+//         std.testing.allocator.free(i);
+//     };
 
-    // Had a bad implementation of strGt (`sort` crashes if gte instead of gt),
-    // make sure that doesn't slip up again.
+//     // Had a bad implementation of strGt (`sort` crashes if gte instead of gt),
+//     // make sure that doesn't slip up again.
 
-    _ = std.sort.sort([]const u8, items[0..], {}, strLt);
-    _ = std.sort.sort([]const u8, items[0..], {}, strGt);
-}
+//     _ = std.sort.sort([]const u8, items[0..], {}, strLt);
+//     _ = std.sort.sort([]const u8, items[0..], {}, strGt);
+// }
+
+// test "Sorting panics on >1023 items" {
+//     var items: [1024]usize = undefined;
+//     for (items) |_, idx| {
+//         items[idx] = idx;
+//     }
+
+//     const impl = struct {
+//         fn lt(_: void, a: usize, b: usize) bool {
+//             return a < b;
+//         }
+//         fn lte(_: void, a: usize, b: usize) bool {
+//             return a <= b;
+//         }
+//     };
+
+//     _ = std.sort.sort(usize, items[0..1023], {}, impl.lt); // ok
+//     _ = std.sort.sort(usize, items[0..1023], {}, impl.lte); // ok
+
+//     _ = std.sort.sort(usize, items[0..], {}, impl.lt); // ok
+//     _ = std.sort.sort(usize, items[0..], {}, impl.lte); // panic!
+// }
 
 fn Proc(comptime WriterType: type) type {
     return struct {
